bb3adf080a6fa854541e7c7d557708a2d3122e4a
shalor1k <rodionandreev2@gmail.com>
2023-12-27 01:01:43 +0500

    первый коммит

---
+++ db.py
@@ -0,0 +1,109 @@
+import psycopg2.extras
+
+conn = psycopg2.connect(dbname='tank_community', user='postgres',
+                        password='AiRa6779', host='localhost')
+
+cur = conn.cursor()
+
+
+async def check_user(user_id: int, username: str) -> bool:
+    cur.execute(f"SELECT id FROM users WHERE tg_id = {user_id}")
+    personal_id = cur.fetchone()
+
+    if personal_id is not None:
+        return True
+
+    else:
+        cur.execute("SELECT MAX(id) FROM users")
+        max_personal_id = cur.fetchone()
+        if max_personal_id[0] is None:
+            max_personal_id = 0
+        else:
+            max_personal_id = max_personal_id[0] + 1
+
+        await add_user(max_personal_id, user_id, username)
+
+        return False
+
+
+async def add_user(personal_id: int, user_id: int, username: str) -> None:
+    try:
+        cur.execute(f"INSERT INTO users (id, tg_id, username) VALUES ({personal_id}, {user_id}, '{username}')")
+        conn.commit()
+
+        cur.execute(f"INSERT INTO users_forms (id, themes) VALUES ({personal_id},"
+                    f" ARRAY[{['Путешествия', 'Еда', 'Работа и карьера', 'Досуг в Москве', 'Спорт и увлечения']}])")
+        conn.commit()
+
+    except Exception as e:
+        conn.rollback()
+        print(f"Ошибка в add_user {e}")
+
+
+async def update_column_in_users_forms(value: str, column: str, user_id: int) -> bool:
+    try:
+        cur.execute(f"SELECT id FROM users WHERE tg_id = {user_id}")
+        personal_id = cur.fetchone()[0]
+        cur.execute(f"UPDATE users_forms SET {column} = '{value}' WHERE id = {personal_id}")
+        conn.commit()
+
+        return True
+
+    except Exception as e:
+        conn.rollback()
+        print(f"Ошибка в update_column_in_users_forms {e}")
+        return False
+
+
+async def select_themes(user_id: int) -> list:
+    cur.execute(f"SELECT id FROM users WHERE tg_id = {user_id}")
+    personal_id = cur.fetchone()[0]
+    cur.execute(f"SELECT themes FROM users_forms WHERE id = {personal_id}")
+    themes_list = cur.fetchone()
+
+    return themes_list[0]
+
+
+async def select_user_from_users_forms_with_match(user_id):
+    cur.execute(f"SELECT id FROM users WHERE tg_id = {user_id}")
+    personal_id = cur.fetchone()[0]
+
+    cur.execute(f"SELECT MIN(id) FROM users_forms WHERE match_flag = 'false' AND id != {personal_id}")
+    user_id = cur.fetchone()[0]
+
+    if user_id is not None:
+        cur.execute(f"SELECT fullname FROM users_forms WHERE id = {user_id}")
+        fullname = cur.fetchone()[0]
+
+        if fullname is not None:
+            return user_id, fullname
+
+        return 0, 0
+
+    return 0, 0
+
+
+async def update_themes(user_id: int, themes: list) -> None:
+    cur.execute(f"SELECT id FROM users WHERE tg_id = {user_id}")
+    personal_id = cur.fetchone()[0]
+
+    cur.execute(f"UPDATE users_forms SET themes = ARRAY[{themes}] WHERE id = {personal_id}")
+    conn.commit()
+
+
+async def select_fullname(user_id: int) -> str:
+    cur.execute(f"SELECT id FROM users WHERE tg_id = {user_id}")
+    personal_id = cur.fetchone()[0]
+
+    cur.execute(f"SELECT fullname FROM users_forms WHERE id = {personal_id}")
+    name = cur.fetchone()[0]
+
+    return name
+
+
+async def select_tg_id(user_id: int) -> int:
+    cur.execute(f"SELECT tg_id FROM users WHERE id = {user_id}")
+    tg_id = cur.fetchone()
+
+    return tg_id[0]
+
