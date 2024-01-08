57ca1e8598bc8df55e50cf015da5e1ebd818606d
shalor1k <rodionandreev2@gmail.com>
2024-01-09 03:19:12 +0500

    final version

--- db.py
+++ db.py
@@ -1,6 +1,6 @@
 import psycopg2.extras
 
-conn = psycopg2.connect(dbname='tank_community', user='postgres',
+conn = psycopg2.connect(dbname='tank_community', user='shalor1k',
                         password='AiRa6779', host='localhost')
 
 cur = conn.cursor()
@@ -31,8 +31,9 @@ async def add_user(personal_id: int, user_id: int, username: str) -> None:
         cur.execute(f"INSERT INTO users (id, tg_id, username) VALUES ({personal_id}, {user_id}, '{username}')")
         conn.commit()
 
-        cur.execute(f"INSERT INTO users_forms (id, themes) VALUES ({personal_id},"
-                    f" ARRAY[{['Путешествия', 'Еда', 'Работа и карьера', 'Досуг в Москве', 'Спорт и увлечения']}])")
+        cur.execute(f"INSERT INTO users_forms (id, themes, matches) VALUES ({personal_id},"
+                    f" ARRAY[{['Путешествия', 'Еда', 'Работа и карьера', 'Досуг в Москве', 'Спорт и увлечения']}], "
+                    f"ARRAY[{999999}])")
         conn.commit()
 
     except Exception as e:
@@ -76,7 +77,11 @@ async def select_user_from_users_forms_with_match(user_id):
         fullname = cur.fetchone()[0]
 
         if fullname is not None:
-            return user_id, fullname
+            cur.execute(f"SELECT matches FROM users_forms WHERE id = {personal_id}")
+            matches = cur.fetchone()[0]
+            if user_id not in matches:
+                return user_id, fullname
+            return 0, 0
 
         return 0, 0
 
@@ -107,3 +112,45 @@ async def select_tg_id(user_id: int) -> int:
 
     return tg_id[0]
 
+
+async def check_car(user_id: int) -> int:
+    cur.execute(f"SELECT id FROM users WHERE tg_id = {user_id}")
+    personal_id = cur.fetchone()[0]
+
+    cur.execute(f"SELECT car FROM users_forms WHERE id = {personal_id}")
+    car = cur.fetchone()[0]
+
+    if car is not None:
+        return 1
+    return 0
+
+
+async def update_match_flag(user_id: int, match_flag: bool) -> None:
+    try:
+        cur.execute(f"SELECT id FROM users WHERE tg_id = {user_id}")
+        personal_id = cur.fetchone()[0]
+
+        cur.execute(f"UPDATE users_forms SET match_flag = '{match_flag}' WHERE id = {personal_id}")
+        conn.commit()
+
+    except Exception as e:
+        conn.rollback()
+        print(f"Ошибка в update_match_flag {e}")
+
+
+async def append_to_matches(user_id: int, interlocutor_id: int) -> None:
+    try:
+        cur.execute(f"SELECT id FROM users WHERE tg_id = {user_id}")
+        personal_id = cur.fetchone()[0]
+
+        cur.execute(f"SELECT id FROM users WHERE tg_id = {interlocutor_id}")
+        interlocutor_personal_id = cur.fetchone()[0]
+
+        cur.execute(f"UPDATE users_forms SET matches = array_append(matches, {interlocutor_personal_id})"
+                    f" WHERE id = {personal_id}")
+
+        conn.commit()
+
+    except Exception as e:
+        conn.rollback()
+        print(f"Ошибка в append_to_matches {e}")
